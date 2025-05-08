b5fcfc65ced38945ca6236c842264fc5c220dad2
SantadHommontha <santadhommontha@gmail.com>
2025-05-08 17:04:55 +0700

    Game Over

--- Main.py
+++ Main.py
@@ -20,10 +20,12 @@ ORANGE = (255, 165, 0)
 ser = serial.Serial('COM5', 115200, timeout=0.1)
 
 # Game variables
+hp = 3
 score = 0
 fruits = []
 FRUIT_SIZE = 100
 fruit_speed = 5
+game_over = False;
 max_fruit_in_screen = 8
 
 fruits_name = [
@@ -68,7 +70,6 @@ class Fruit:
 
 # Function
 def Generate_Fruit():
-    global fruits
     if len(fruits) >= max_fruit_in_screen :
         return
     
@@ -76,15 +77,15 @@ def Generate_Fruit():
         Create_Fruit()
     
 
-    if random.randint(0,30) == 1:
-        Create_Fruit()
+    else:
+        if random.randint(0,30) == 1:
+            Create_Fruit()
         
        
 def Create_Fruit():
-    #global RED,YELLOW,ORANGE
-    global WIDTH,fruit_image,fruits_name
+   
     x = random.randint(0, WIDTH - FRUIT_SIZE)
-    #color = random.choice([RED, YELLOW, ORANGE])
+   
     name = random.choice(fruits_name)
     speed = random.choice([5,7,8])
     fruits.append(Fruit(x, 0,speed,name,fruit_image[name]))
@@ -98,7 +99,7 @@ def Micro_Bit_Serial():
       
 
 def Check_Fruit(input):
-    global RED,YELLOW,ORANGE,fruits,score
+    global fruits,score
     if len(fruits) == 0 : 
         return
 
@@ -122,45 +123,96 @@ def Check_Fruit(input):
 
 
 def Update_Fruit():
-    global fruits,HEIGHT
+    global fruits,hp
     for fruit in fruits[:]:
         fruit.fall()
         fruit.draw()    
         if fruit.Get_Y_Position() > HEIGHT:
             fruits.remove(fruit)
+            hp -= 1
 
 
 def DisplayScore():
-    global score
+    
     font = pygame.font.SysFont(None, 36)
     score_text = font.render(f"Score: {score}",True,BLACK)
     screen.blit(score_text,(10,10))
-# Main game loop
+
+def DiaplayHP():
+    font = pygame.font.SysFont(None,36)
+    hp_text = font.render(f"HP: {hp}",True,BLACK)
+    text_width,text_height = hp_text.get_size()
+    screen.blit(hp_text,(WIDTH - text_width - 10,10))
+
+def GameOver():
+    screen.fill(BLACK)
+    font = pygame.font.SysFont(None, 100)
+    gameOver = font.render(f"Game Over",True,WHITE)
+    text_width, text_height = gameOver.get_size()
+    screen.blit(gameOver,((WIDTH / 2) - (text_width / 2 ), HEIGHT / 5))
+
+    font = pygame.font.SysFont(None, 60)
+    gameOver = font.render(f"Score: {score}",True,WHITE)
+    text_width, text_height = gameOver.get_size()
+    screen.blit(gameOver,((WIDTH / 2) - (text_width / 2 ), HEIGHT / 3))
+
+
+    font = pygame.font.SysFont(None, 50)
+    gameOver = font.render("Touch Banana To Play Again",True,WHITE)
+    text_width, text_height = gameOver.get_size()
+    screen.blit(gameOver,((WIDTH / 2) - (text_width / 2 ), HEIGHT / 2))
+
+    if ser.in_waiting > 0:
+        command = ser.readline().decode().strip()
+        if command == "B":
+            SetUpGame()
+
+
+
+def SetUpGame():
+    global hp,score,fruits,game_over
+    hp = 3
+    score = 0
+    fruits = []
+    game_over = False
+
+
 running = True
 while running:
-    screen.fill(WHITE)
+    if not game_over :
+        screen.fill(WHITE)
 
-    # Create fruits
-    Generate_Fruit()
+        # Create fruits
+        Generate_Fruit()
 
-    # Read Micro:bit input
-    Micro_Bit_Serial()
+        # Read Micro:bit input
+        Micro_Bit_Serial()
 
-    # Update  fruits
-    Update_Fruit()
+        # Update  fruits
+        Update_Fruit()
        
 
-    # Display score
-    # font = pygame.font.SysFont(None, 36)
-    # score_text = font.render(f"Score: {score}", True, BLACK)
-    # screen.blit(score_text, (10, 10))
-    DisplayScore()
+        # Display UI
+        DisplayScore()
+        DiaplayHP()
+
+    else:
+        GameOver()
+
+
+    # Chack HP
+    if hp <= 0:
+        game_over = True
 
     # Event handling
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_SPACE:
+                GameOver()
 
+    #Update Screen
     pygame.display.flip()
     clock.tick(30)
 
